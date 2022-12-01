7b6e995c33074d05f9e3fc0b7dd8bcbdd34cf254
kyh <kwonyong6@gmail.com>
2022-12-02 01:18:07 +0900

    Update httpInstance.ts

--- src/api/httpInstance.ts
+++ src/api/httpInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpInstance = axios.create({
-   baseURL: 'https://k-dnc.vercel.app',
+   baseURL: 'https://k-dnc.vercel.app'
 });
 
 export default httpInstance;
