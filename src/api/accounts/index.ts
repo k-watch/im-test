6306eb11e7ed2f4e3439596b6a68c4e6eec15659
kyh <kwonyong6@gmail.com>
2022-12-02 01:35:30 +0900

    1
    1

--- src/api/accounts/index.ts
+++ src/api/accounts/index.ts
@@ -1,3 +1,4 @@
+
 import { IAccount } from '@src/models/IAccount';
 import { IUser } from '@src/models/IUser';
 import { QueriesParmas } from '@src/types';
@@ -9,21 +10,19 @@ import httpInstance from '../httpInstance';
 export const reqGetAccountList = async (queries: QueriesParmas) => {
   const token = cookieInstance.get('token');
 
-  const { headers, data } = await httpInstance.get<IAccount[]>('/accounts',
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      params: {
-        _page: queries.page,
-        _limit: PAGE_LIMIT,
-        brokerId: queries.broker,
-        status: queries.status,
-        isActive: queries.isActive,
-        q: queries.q,
-      },
-    }
-  );
+  const { headers, data } = await httpInstance.get<IAccount[]>('/accounts', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params: {
+      _page: queries.page,
+      _limit: PAGE_LIMIT,
+      brokerId: queries.broker,
+      status: queries.status,
+      isActive: queries.isActive,
+      q: queries.q,
+    },
+  });
 
   return {
     totalCount: headers['x-total-count'] || 0,
